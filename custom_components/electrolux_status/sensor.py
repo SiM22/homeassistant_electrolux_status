7afe9e959778598bd2d75fef679e7d8db6f35fcd
Mauro Midolo <mauro.midolo@hotmail.com>
2022-03-22 17:30:54 +0100

    Added SensorEntity extention class

--- custom_components/electrolux_status/sensor.py
+++ custom_components/electrolux_status/sensor.py
@@ -5,10 +5,13 @@ from .const import DOMAIN
 from .const import SENSOR
 from .entity import ElectroluxStatusEntity
 
+from . import ElectroluxStatusDataUpdateCoordinator
+from homeassistant.components.sensor import SensorEntity
+
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+    coordinator: ElectroluxStatusDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     appliances = coordinator.data.get('appliances', None)
 
     if appliances is not None:
@@ -21,14 +24,14 @@ async def async_setup_entry(hass, entry, async_add_devices):
             )
 
 
-class ElectroluxStatusSensor(ElectroluxStatusEntity):
+class ElectroluxStatusSensor(ElectroluxStatusEntity, SensorEntity):
     """Electrolux Status Sensor class."""
 
     @property
-    def state(self):
+    def native_value(self):
         """Return the state of the sensor."""
         return self.get_entity.state
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self):
         return cast(ApplianceSensor, self.get_entity).unit
