56ef65ec2bf7daad1c361e1c96d72c5b416354bd
Mauro Midolo <mauro.midolo@hotmail.com>
2023-09-13 08:28:00 +0200

    Added tokenFileName parameter to login

--- custom_components/electrolux_status/pyelectroluxconnect_util.py
+++ custom_components/electrolux_status/pyelectroluxconnect_util.py
@@ -5,5 +5,5 @@ class pyelectroluxconnect_util:
     @staticmethod
     def get_session(username, password, region="emea", language ="eng"):
         return pyelectroluxconnect.Session(
-            username, password, deviceId="ElectroluxHomeAssistant", language=language, region=region,
+            username, password, deviceId="ElectroluxHomeAssistant", tokenFileName=username, language=language, region=region,
         )
